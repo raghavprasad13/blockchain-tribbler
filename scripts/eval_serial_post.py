c9daf5fbcb80e8a7a8b84dff3a6d9556d7df18a6
Rajdeep Pinge <rajpinge@gmail.com>
2022-06-10 16:42:19 -0700

    Modified code, data collection and plotting

--- scripts/eval_serial_post.py
+++ scripts/eval_serial_post.py
@@ -23,6 +23,7 @@ import argparse
 
 def run(gas_price: int, num_tribs: int, account, timestamp):
     network.gas_price(str(gas_price) + " gwei")
+    # network.gas_limit(134440*0.95)
     # account = accounts[6]
 
     tribbler = TribblerMain(
@@ -40,12 +41,24 @@ def run(gas_price: int, num_tribs: int, account, timestamp):
     # else:
     #    user = "harsh"
 
-    users = ["paralleluser1"]
+    users = ["serialuser1", "serialuser2", "serialuser3", "serialuser4", "serialuser5"]
 
     user = users[0]
+
+    if gas_price == 10:
+        user = users[0]
+    elif gas_price == 20:
+        user = users[1]
+    elif gas_price == 30:
+        user = users[2]
+    elif gas_price == 40:
+        user = users[3]
+    else:
+        user = users[4]
+
     # print(user)
 
-    tribbler.signupTx(user)
+    # tribbler.signupTx(user)
 
     file_name = "new_parallel_post"
 
@@ -63,6 +76,8 @@ def run(gas_price: int, num_tribs: int, account, timestamp):
         time_taken = round(end - start, 3)
         f.write(str(trib_num) + "," + str(time_taken) + "\n")
 
+        time.sleep(1)
+
     f.close()
 
 
@@ -76,8 +91,9 @@ def main():
 
     # gas_prices = [GAS_PRICE_SAFE_LOW, GAS_PRICE_STANDARD, GAS_PRICE_FAST]
     # gas_prices = [GAS_PRICE_FAST, GAS_PRICE_STANDARD, GAS_PRICE_SAFE_LOW]
-    gas_prices = [GAS_PRICE_COMMON]
-    num_tribs = 5
+    # gas_prices = [GAS_PRICE_COMMON]
+    gas_prices = [30]
+    num_tribs = 105
     for gas_price in gas_prices:
         run(
             gas_price=gas_price,
--- scripts/plot_serial_post_data.py
+++ scripts/plot_serial_post_data.py
@@ -4,67 +4,126 @@
 import csv
 import os
 
+import statistics
+
+import numpy as np, scipy.stats as st
+
+# import statsmodels.stats.api as sms
+
 
 def main():
     pwd = os.getcwd()
 
-    fast_all = []
-    med_all = []
-    slow_all = []
+    file_names = [
+        "post_10.csv",
+        "post_20.csv",
+        "post_30.csv",
+        "post_40.csv",
+        "post_50.csv",
+    ]
 
-    gas_fast_dict = []
-    gas_medium_dict = []
-    gas_slow_dict = []
+    for filename in file_names:
+        gas_price = filename.split(".")[0].split("_")[1]
+        data_all = []
 
-    with open(pwd + "/data/serial_post_36_1654626205_.csv") as csvfile:
-        csvdata = csv.reader(csvfile)
+        with open(pwd + "/data_remote/" + filename) as csvfile:
+            csvdata = csv.reader(csvfile)
 
-        for row in csvdata:
-            # print(row)
-            rowindex = int(row[0])
-            rowval = float(row[1])
+            for row in csvdata:
+                # print(row)
+                if len(row) == 0:
+                    break
+                elif len(row) == 1:
+                    rowval = float(row[0])
+                else:
+                    # rowindex = int(row[0])
+                    rowval = float(row[1])
 
-            fast_all.append(rowval)
+                data_all.append(rowval)
 
-            gas_fast_dict.append((rowindex, rowval))
+                # gas_fast_dict.append((rowindex, rowval))
 
-    # print(gas_fast_dict)
+        # print(data_all)
+        print("Gas price: {} gwei".format(gas_price))
+        print("Num data points: {}".format(len(data_all)))
+        print(
+            "Mean: {}, stdev: {}".format(
+                statistics.mean(data_all), statistics.stdev(data_all)
+            )
+        )
 
-    with open(pwd + "/data/serial_post_30_1654626205_.csv") as csvfile:
-        csvdata = csv.reader(csvfile)
+        print(
+            "95% CI: {}".format(
+                st.t.interval(
+                    0.95,
+                    len(data_all) - 1,
+                    loc=np.mean(data_all),
+                    scale=st.sem(data_all),
+                )
+            )
+        )
 
-        for row in csvdata:
-            # print(row)
-            rowindex = int(row[0])
-            rowval = float(row[1])
+        print()
 
-            med_all.append(rowval)
+        # print(sms.DescrStatsW(data_all).tconfint_mean())
 
-            gas_medium_dict.append((rowindex, rowval))
+    # fast_all = []
+    # med_all = []
+    # slow_all = []
 
-    # print(gas_medium_dict)
+    # gas_fast_dict = []
+    # gas_medium_dict = []
+    # gas_slow_dict = []
 
-    with open(pwd + "/data/serial_post_27_1654626205_.csv") as csvfile:
-        csvdata = csv.reader(csvfile)
+    # with open(pwd + "/data/serial_post_36_1654626205_.csv") as csvfile:
+    #     csvdata = csv.reader(csvfile)
 
-        for row in csvdata:
-            # print(row)
-            rowindex = int(row[0])
-            rowval = float(row[1])
+    #     for row in csvdata:
+    #         # print(row)
+    #         rowindex = int(row[0])
+    #         rowval = float(row[1])
 
-            slow_all.append(rowval)
+    #         fast_all.append(rowval)
 
-            gas_slow_dict.append((rowindex, rowval))
+    #         gas_fast_dict.append((rowindex, rowval))
+
+    # # print(gas_fast_dict)
+
+    # with open(pwd + "/data/serial_post_30_1654626205_.csv") as csvfile:
+    #     csvdata = csv.reader(csvfile)
+
+    #     for row in csvdata:
+    #         # print(row)
+    #         rowindex = int(row[0])
+    #         rowval = float(row[1])
+
+    #         med_all.append(rowval)
+
+    #         gas_medium_dict.append((rowindex, rowval))
+
+    # # print(gas_medium_dict)
+
+    # with open(pwd + "/data/serial_post_27_1654626205_.csv") as csvfile:
+    #     csvdata = csv.reader(csvfile)
+
+    #     for row in csvdata:
+    #         # print(row)
+    #         rowindex = int(row[0])
+    #         rowval = float(row[1])
+
+    #         slow_all.append(rowval)
+
+    #         gas_slow_dict.append((rowindex, rowval))
 
     # print(gas_slow_dict)
 
-    import statistics
+    # import statistics
 
-    print(statistics.mean(fast_all), statistics.stdev(fast_all))
+    # print(statistics.mean(fast_all), statistics.stdev(fast_all))
 
-    print(statistics.mean(med_all), statistics.stdev(med_all))
+    # print(statistics.mean(med_all), statistics.stdev(med_all))
 
-    print(statistics.mean(slow_all), statistics.stdev(slow_all))
+    # print(statistics.mean(slow_all), statistics.stdev(slow_all))
 
     # import matplotlib.pyplot as plt
 
